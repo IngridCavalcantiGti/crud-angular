0f4081ff6f91c7bad164615a4c2d71b3cb25a229
Ingrid Cavalcanti <ingridgti08@hotmail.com>
2020-09-23 20:42:37 -0300

    primeiro commit

---
+++ projects/ngx-data-table/src/lib/ngx-data-table.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-data-table',
+  template: `
+    <p>
+      ngx-data-table works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NgxDataTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/ngx-data-table/src/lib/ngx-data-table.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgxDataTableComponent } from './ngx-data-table.component';
+
+
+
+@NgModule({
+  declarations: [NgxDataTableComponent],
+  imports: [
+  ],
+  exports: [NgxDataTableComponent]
+})
+export class NgxDataTableModule { }
